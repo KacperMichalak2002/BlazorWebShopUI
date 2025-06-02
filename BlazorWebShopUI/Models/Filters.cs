0a4c8b35c92a7d7629d0e9eb89b318b7a6057fe0
Kacper Michalak <michalakkacper2002@gmail.com>
2025-06-02 21:51:48 +0200

    Added basic filtering option based on the product specification

---
+++ BlazorWebShopUI/Models/Filters.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWebShopUI.Models
+{
+    public class Filters
+    {
+        public List<String> Categories { get; set; } = new List<string>();
+        public Dictionary<String, List<String>> FiltersToCreate { get; set; } = new Dictionary<string, List<string>>();
+    }
+}
