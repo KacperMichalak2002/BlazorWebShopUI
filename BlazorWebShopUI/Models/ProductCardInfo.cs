13d3bac5559947ddec0101179042b6eea78c592d
Kacper Michalak <michalakkacper2002@gmail.com>
2025-06-02 16:28:34 +0200

    Added chosen category page to show only products within the chosen category

--- BlazorWebShopUI/Models/ProductCardInfo.cs
+++ BlazorWebShopUI/Models/ProductCardInfo.cs
@@ -1,8 +1,9 @@
-namespace BlazorWebShop.Models
+namespace BlazorWebShopUI.Models
 {
     public class ProductCardInfo
     {
         public string Title { get; set; } = string.Empty;
+        public List<String> Category { get; set; } = new List<string>();
         public List<String> Description { get; set; } = new List<string>();
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
