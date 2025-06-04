1bc2b06ad7287707085fea0bb69ef34d9b667ef6
Kacper Michalak <michalakkacper2002@gmail.com>
2025-06-04 21:56:13 +0200

    Added product page

--- BlazorWebShopUI/Models/ProductCardInfo.cs
+++ BlazorWebShopUI/Models/ProductCardInfo.cs
@@ -2,11 +2,15 @@ namespace BlazorWebShopUI.Models
 {
     public class ProductCardInfo
     {
+
+        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public List<String> Category { get; set; } = new List<string>();
         public List<String> Description { get; set; } = new List<string>();
         public Dictionary<string, string> Specification { get; set; } = new Dictionary<string, string>();
         public decimal Price { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
+
+        public List<String> images { get; set; } = new List<string>();
     }
 }
