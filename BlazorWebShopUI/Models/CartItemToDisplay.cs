283ba674f5cdacb4fb5626be0ae5624f93f29c56
Kacper Michalak <michalakkacper2002@gmail.com>
2025-06-05 20:43:28 +0200

    Changing amount in cart changes the final value

---
+++ BlazorWebShopUI/Models/CartItemToDisplay.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWebShopUI.Models
+{
+    public class CartItemToDisplay
+    {
+        public string ImageUrl { get; set; } = string.Empty;
+        public string cartItemName { get; set; } = string.Empty;
+        public float cartItemPrice { get; set; }
+        public int amount { get; set; }
+        public int id { get; set; }
+    }
+}
