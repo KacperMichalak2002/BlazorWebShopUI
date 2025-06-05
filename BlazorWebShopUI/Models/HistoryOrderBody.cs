9d02867d094b7332aad88dbf79a0446c64692e56
Kacper Michalak <michalakkacper2002@gmail.com>
2025-06-05 21:46:11 +0200

    Added functionality to finalizing purchase and saving it in the orders history

---
+++ BlazorWebShopUI/Models/HistoryOrderBody.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWebShopUI.Models
+{
+    public class HistoryOrderBody
+    {
+        public List<String> imagesUrl { get; set; } = new List<String>();
+        public float totalPrice { get; set; }
+
+        public String date { get; set; } = DateTime.Now.ToShortDateString();
+    }
+}
